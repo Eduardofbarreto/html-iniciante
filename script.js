4d9005dff166d179c5b406f75eada4ac2058b906
Eduardofbarreto <fbarreto.eduardo>
2025-07-13 21:58:08 -0300

    arrumando algumas formatações

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-let numeroSecreto = Math.floor(Math.random() * 10) + 1;
-let tentativas = 0;
+let x = prompt("Digite um valor: ");
+let y = prompt("Digite o segundo valor: ");
 
-const inputPalpite = document.getElementById("inputPalpite");
-const btnVerificar = document.getElementById("btnVerificar");
-const elementoResultado = document.getElementById("resultado");
+let soma = parseInt(x) + parseInt(y);
 
-function verificarPalpite(){
-    let palpite = Number(inputPalpite.value);
-    tentativas++;
-
-    if(palpite < 1 || palpite > 10 || isNaN(palpite)){
-        elementoResultado.textContent = "Por favor digite um número válido!";
-        elementoResultado.style.color = "orange";
-        return;
-    }
-    if(palpite === numeroSecreto){
-        elementoResultado.textContent = `Parabéns, você acertou!`;
-        elementoResultado.style.color = "green";
-        inputPalpite.disabled = true;
-        btnVerificar.disabled = true;
-    }else if(palpite < numeroSecreto){
-        elementoResultado.textContent = "Tente um valor mais alto!";
-        elementoResultado.style.color = "red";
-    }else{
-        elementoResultado.textContent = "Tente um valor mais baixo!";
-        elementoResultado.style.color = "blue";
-    }
-    inputPalpite.value = "";
-    inputPalpite.focus();
-}
-
-btnVerificar.addEventListener("click", verificarPalpite);
-
-inputPalpite.addEventListener("keypress", function(event){
-    if(event.key === "Enter"){
-        verificarPalpite();
-    }
-});
+console.log("O resultado da soma é: " + soma);
