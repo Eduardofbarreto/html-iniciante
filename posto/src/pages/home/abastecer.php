c3b5f5739341521b75a8ea3f27b1adebd3bee534
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-02 18:24:48 -0300

    seguindo no backend

--- posto/src/pages/home/abastecer.php
+++ posto/src/pages/home/abastecer.php
@@ -1,26 +1,25 @@
 <?php
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    // Define the fuel prices in an associative array
+
     $precos = [
-        'gasolina' => 5.50,   // Example price for gasoline
-        'etanol' => 3.99,     // Example price for ethanol
-        'diesel' => 4.80,     // Example price for diesel
-        'diesels10' => 4.95,  // Example price for diesel S10
-        'gas' => 4.20         // Example price for gas
+        'gasolina' => 5.69,
+        'gasolina_adt' => 5.85,
+        'etanol' => 4.35,    
+        'diesel' => 5.59,     
+        'diesels10' => 5.79,  
+        'gas' => 4.20         
     ];
 
     if(isset($_POST['combustivel']) && isset($_POST['litro'])){
 
         $combustivel = $_POST['combustivel'];
         $litro = $_POST['litro'];
-        
-        // Check if the selected fuel exists in our prices array
+
         if(array_key_exists($combustivel, $precos)){
             // Get the price for the selected fuel
             $preco_por_litro = $precos[$combustivel];
             
-            // Calculate the total value
             $valor_final = $litro * $preco_por_litro;
 
             echo "Foi abastecido " . $litro . " litros de " . $combustivel . ".<br>";
